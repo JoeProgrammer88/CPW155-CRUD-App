8fd7bc42c791405d88c273bdae7328445c31f1b1
joeprogrammer88 <JoeProgrammer88@users.noreply.github.com>
2026-05-20 13:41:15 -0700

    Add TennisRacketDb class with CRUD method stubs
    Introduced TennisRacketDb static class in CPW155_CRUD_App namespace.
    Added method stubs for CRUD operations on TennisRacket objects.
    All methods currently throw NotImplementedException.

---
+++ CPW155-CRUD-App/TennisRacketDb.cs
@@ -0,0 +1,29 @@
+﻿namespace CPW155_CRUD_App;
+
+public static class TennisRacketDb
+{
+    public static List<TennisRacket> GetAllTennisRackets()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void AddTennisRacket(TennisRacket racket)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void UpdateTennisRacket(TennisRacket racket)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void DeleteTennisRacket(int itemId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void DeleteTennisRacket(TennisRacket racket)
+    {
+        throw new NotImplementedException();
+    }
+}
