fc5896a2d4dd8e9f39c0446c90908bb03b830e04
joeprogrammer88 <JoeProgrammer88@users.noreply.github.com>
2026-05-20 14:57:09 -0700

    Add SQL Server support and implement GetAllTennisRackets
    Added Microsoft.Data.SqlClient package for database access. Implemented GetAllTennisRackets to retrieve all tennis racket records from the database and return them as a list. Included XML documentation for the method.

--- CPW155-CRUD-App/TennisRacketDb.cs
+++ CPW155-CRUD-App/TennisRacketDb.cs
@@ -1,10 +1,56 @@
-﻿namespace CPW155_CRUD_App;
+﻿using Microsoft.Data.SqlClient;
+
+namespace CPW155_CRUD_App;
 
 public static class TennisRacketDb
 {
+    /// <summary>
+    /// Retrieves all tennis rackets. If no rackets are found,
+    /// an empty list is returned.
+    /// </summary>
+    /// <returns>A list of all tennis rackets.</returns>
     public static List<TennisRacket> GetAllTennisRackets()
     {
-        throw new NotImplementedException();
+        // Get a connection to the database
+        SqlConnection con = new();
+        //todo: Simplify and understand the connection string
+        con.ConnectionString = "Data Source=localhost;Initial Catalog=TennisDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
+        con.Open();
+
+        // Create a SQL command to select all tennis rackets
+        SqlCommand selectCmd = new();
+        selectCmd.Connection = con;
+        selectCmd.CommandText = 
+            "SELECT ItemId" +
+            ", Weight" +
+            ", HeadSize" +
+            ", Brand" +
+            ", Price" +
+            " FROM TennisRacket";
+
+        // Execute the command and read the results
+        SqlDataReader reader = selectCmd.ExecuteReader();
+
+        List<TennisRacket> rackets = new();
+
+        // For each racket, create object and add it to the list
+        while (reader.Read())
+        {
+            TennisRacket racket = new()
+            {
+                ItemId = Convert.ToInt32(reader["ItemId"]),
+                Weight = Convert.ToByte(reader["Weight"]),
+                HeadSize = Convert.ToInt16(reader["HeadSize"]),
+                Brand = reader["Brand"].ToString(),
+                Price = Convert.ToDouble(reader["Price"])
+            };
+            rackets.Add(racket);
+        }
+
+        // Close the connection
+        con.Dispose();
+
+        return rackets;
     }
 
     public static void AddTennisRacket(TennisRacket racket)
